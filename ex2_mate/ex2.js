b75317dce6efb29fda55b8c8b74e74dc41e73999
slimanxio <slimanxio@gmail.com>
2019-01-17 13:42:27 +0100

    ex2 hechosdas

--- ex2_mate/ex2.js
+++ ex2_mate/ex2.js
@@ -4,35 +4,35 @@
 	Usado: parseInt(), Math.random(), Math.round()
 */
 
-document.write('<img src="../logo.png" alt="Algo falla no logo">');
-document.write('<h1> Páxina de mates que usa .css </h1>')
+document.write('<img src="../logoAnxo.jpg" alt="Algo falla no logo">');
+document.write('<h1> Hoja de calculadora .css </h1>')
 
 // as variables decláranse
 var num1; 
 var num2;
 
 // pregunto o valor ó usuario; Javscript considéraos caracteres, non números
-num1 = prompt('Canto vale o primeiro número? ');
-num2 = prompt('Canto vale o segundo número? ');
+num1 = prompt('¿Primer número? ');
+num2 = prompt('¿Segundo número? ');
 
 // convertimos a números
 var solucionado = parseInt(num1)+parseInt(num2);
-document.write('O resultado da túa suma é '+ solucionado);
+document.write('La suma de los numeros que me has dado es: '+ solucionado);
 
 // fallos nos cálculos
 var num3 = 0;
-document.write('<br> <hr>Atención ós erros nas contas (dividir entre 0) será...'+ num1/num3); // opsssss
+document.write('<br> <hr>El resutado de tu suma entre 0 será...'+ solucionado/num3); // opsssss
 
 
-document.write('<hr><h3>Javascript "inventa" un número entre 0 e 10</h3>');
+document.write('<hr><h3>Javascript busca un número entre 0 e 5</h3>');
 
 var misterio = Math.random();
 document.write('<br>Xenero un número aleatorio menor ca un: '+ misterio);
 
 //olliño con este truquito para non crear máis variables
-misterio = misterio*10;
-document.write('<br> Agora está entre 0 e 10: '+ misterio);
+misterio = misterio*5;
+document.write('<br> Agora está entre 0 e 5: '+ misterio);
 
 //olliño con este truquito para non crear máis variables (outra vez)
 misterio = Math.round(misterio)
-document.write('<br> Redondeo para non andar con decimais '+ misterio);
+document.write('<br> Redondeo para no andar con decimales '+ misterio);
